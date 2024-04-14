a468dbdb5fef63e6985821597325cda0eacae7dd
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2024-04-14 22:51:12 +0200

    feat: add production connectionj to sequeliza cli

--- src/databases/config/sequelize-config.cjs
+++ src/databases/config/sequelize-config.cjs
@@ -12,10 +12,7 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
-    username: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    port: Number(process.env.POSTGRES_PORT),
   },
 };
