a50a1ebd74ca21d1e89e31969bdc164c6e7fb34b
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2024-04-16 20:53:48 +0200

    feat: seed initial data

--- src/seeders/seeder.module.ts
+++ src/seeders/seeder.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { SeederService } from './seeder.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Bank } from 'src/accounts/models/bank.model';
+import { Currency } from 'src/accounts/models/currency.model';
 import { AppSequelizeModule } from 'src/databases/app-sequelize.module';
-import ExpenseModule from 'src/expenses/expense.module';
+import { SeederService } from './seeder.service';
+import { Account } from 'src/accounts/models/account.model';
 
 @Module({
-  imports: [AppSequelizeModule, ExpenseModule],
+  imports: [
+    AppSequelizeModule,
+    SequelizeModule.forFeature([Currency, Bank, Account]),
+  ],
   providers: [SeederService],
 })
 export class SeederModule {}
--- src/seeders/seeder.service.ts
+++ src/seeders/seeder.service.ts
@@ -1,22 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { ExpenseService } from 'src/expenses/services/expense.service';
-import MOCK_EXPENSES from './data/expenses';
+import { InjectModel } from '@nestjs/sequelize';
+import { Account } from 'src/accounts/models/account.model';
+import { Bank } from 'src/accounts/models/bank.model';
+import { Currency } from 'src/accounts/models/currency.model';
 
 @Injectable()
 export class SeederService {
-  constructor(private readonly expenseService: ExpenseService) {}
+  constructor(
+    @InjectModel(Currency) private readonly currencyModel: typeof Currency,
+    @InjectModel(Bank) private readonly bankModel: typeof Bank,
+    @InjectModel(Account) private readonly accountModel: typeof Account,
+  ) {}
 
   async seed() {
-    await this.seedExpenses();
-  }
+    // Currencies
+    const eu = await this.currencyModel.create({ name: 'Euro', symbol: '€' });
+    await this.currencyModel.create({ name: 'Dolar', symbol: '$' });
 
-  async seedExpenses() {
-    try {
-      for (const expense of MOCK_EXPENSES) {
-        await this.expenseService.create(expense);
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    // Bank
+    const bbva = await this.bankModel.create({
+      name: 'BBVA',
+      description: 'España',
+    });
+
+    // Accounts
+    await this.accountModel.create({
+      name: 'Nómina',
+      bankId: bbva.id,
+      currencyId: eu.id,
+      amount: 0,
+    });
+    await this.accountModel.create({
+      name: 'Ahorro',
+      bankId: bbva.id,
+      currencyId: eu.id,
+      amount: 0,
+    });
   }
 }
